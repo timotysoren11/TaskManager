c9a2e654065c441c0d0ea00c042a5820f94d8b0b
Timoty Soren <timotysoren03@gmail.com>
2026-06-10 09:13:29 +0530

    configure production API URL frontend

--- frontend/src/api/taskApi.js
+++ frontend/src/api/taskApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const taskApi = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 taskApi.interceptors.request.use((config) => {
