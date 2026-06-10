50a91a73537bc4be88965b48f028f5f755183d09
Timoty Soren <timotysoren03@gmail.com>
2026-06-10 09:02:50 +0530

    configure production API URL

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,8 +1,7 @@
-
 import axios from "axios";
 
 const authApi = axios.create({
-  baseURL: "http://localhost:5000/api/auth",
+  baseURL: `${import.meta.env.VITE_API_URL}/api/auth`,
 });
 
 export default authApi;
